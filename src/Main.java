b1c16c7f4b5df79ba182f89e5f9208b72de439f7
Jayden <jaydencruz824@gmail.com>
2025-01-31 18:04:55 -0600

    implemented SelectionSort

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,17 @@ public class Main {
         //Bubble Sort
         Sortable bubble = new BubbleSort();
         int[] sortedBubble = bubble.sorty(unsortedArr);
-        System.out.print(Arrays.toString(sortedBubble));
+        System.out.print("Bubble Sort: " +Arrays.toString(sortedBubble));
 
         //Insertion Sort
         Sortable insertion = new InsertionSort();
         int[] sortedInsertion = insertion.sorty(unsortedArr);
-        System.out.print(Arrays.toString(sortedInsertion));
+        System.out.print("\nInsertion Sort: " + Arrays.toString(sortedInsertion));
 
+        //Selection Sort
+        Sortable selection = new SelectionSort();
+        int[] sortedSelection = selection.sorty(unsortedArr);
+        System.out.print("\nSelection Sort: " + Arrays.toString(sortedSelection));
 
     }
 }
